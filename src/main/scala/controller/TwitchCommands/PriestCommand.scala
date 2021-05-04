a5c3aaed340557a539ee71bee9fcc3dccf0441c0
PriestPope <nawarkhouri1@gmail.com>
2021-05-04 04:41:18 +0300

    Added the !Priest command and tested it by using main method and connecting to the twitch chat.

--- src/main/scala/controller/TwitchCommands/PriestCommand.scala
+++ src/main/scala/controller/TwitchCommands/PriestCommand.scala
@@ -12,7 +12,7 @@ class PriestCommand(_twitchAPI: TwitchAPI) extends TwitchCommandContract(_twitch
   }
 
   override def executeCommand(chatMessage: ChatMessage, twitchBot: ActorRef): Unit = {
-    twitchAPI.sendRawMessageInChat(chatMessage.username + ", the score is 2 or Priest and 0 for Jesse. Priest remains victorious even after he graduated 116.")
+    twitchAPI.sendRawMessageInChat(chatMessage.username + ", the score is 2 for Priest and 0 for Jesse. Priest remains victorious even after he graduated 116.")
   }
 
 }
